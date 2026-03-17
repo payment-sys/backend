b79d4ea905afcdf4e0f9f550a0524df4e19689db
chungjeongsu <wjdwlgh2000@gmail.com>
2026-03-17 20:09:47 +0900

    프로젝트 생성입니다.

---
+++ src/test/java/com/v_payment/pay/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v_payment.pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
