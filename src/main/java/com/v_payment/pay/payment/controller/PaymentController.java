4f021a111c48a5595f21cc264b78ddb7fa42e310
jeongjiho <wjdwlgh2000@naver.com>
2026-07-18 18:51:45 +0900

    feat: Payment 비동기 + 가상쓰레드 수행 위해, CompletableFutrue 구현

--- src/main/java/com/v_payment/pay/payment/controller/PaymentController.java
+++ src/main/java/com/v_payment/pay/payment/controller/PaymentController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
+
 @Slf4j(topic = "API_LOGGER")
 @RestController
 @RequestMapping("/payments")
@@ -18,7 +20,7 @@ public class PaymentController {
     private final PaymentServiceFacade paymentServiceFacade;
 
     @PostMapping("/approvals")
-    public ApprovalRes approve(
+    public CompletableFuture<ApprovalRes> approve(
             @RequestBody ApprovalReq approvalReq
     ) {
         return paymentServiceFacade.approvePipeline(approvalReq);
