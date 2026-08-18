a933781b16b136350b79c8bbf25bdae76c4b7854
jeongjiho <wjdwlgh2000@naver.com>
2026-08-18 19:39:04 +0900

    refactor: RestClient를 apacheHttpClient로 변경하여, 가상쓰레드를 이용하는 Blocking I/O 모델로 변경

--- src/main/java/com/v_payment/pay/payment/config/TossPaymentClientConfig.java
+++ src/main/java/com/v_payment/pay/payment/config/TossPaymentClientConfig.java
@@ -1,11 +1,14 @@
 package com.v_payment.pay.payment.config;
 
-import com.v_payment.pay.payment.entity.Payment;
-
 import lombok.RequiredArgsConstructor;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.restclient.autoconfigure.RestClientBuilderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
@@ -19,16 +22,45 @@ public class TossPaymentClientConfig {
     private final RestClientBuilderConfigurer restClientBuilderConfigurer;
 
     @Bean
-    public RestClient tossPaymentClient() {
+    public RestClient tossPaymentClient(
+            @Qualifier("apacheRequestFactory")
+            ClientHttpRequestFactory requestFactory
+    ) {
+        return restClientBuilderConfigurer.configure(RestClient.builder())
+                .requestFactory(requestFactory)
+                .build();
+    }
+
+    @Bean("jdkRequestFactory")
+    public ClientHttpRequestFactory jdkRequestFactory() {
         HttpClient httpClient = HttpClient.newBuilder()
                 .connectTimeout(Duration.ofSeconds(tossPaymentProperties.timeout()))
                 .build();
 
-        JdkClientHttpRequestFactory factory = new JdkClientHttpRequestFactory(httpClient);
-        factory.setReadTimeout(Duration.ofSeconds(tossPaymentProperties.timeout()));
+        JdkClientHttpRequestFactory factory =
+                new JdkClientHttpRequestFactory(httpClient);
 
-        return restClientBuilderConfigurer.configure(RestClient.builder())
-                .requestFactory(factory)
+        factory.setReadTimeout(
+                Duration.ofSeconds(tossPaymentProperties.timeout())
+        );
+
+        return factory;
+    }
+
+    @Bean("apacheRequestFactory")
+    public ClientHttpRequestFactory apacheRequestFactory() {
+        CloseableHttpClient httpClient = HttpClients.custom()
                 .build();
+
+        HttpComponentsClientHttpRequestFactory factory =
+                new HttpComponentsClientHttpRequestFactory(httpClient);
+
+        Duration timeout =
+                Duration.ofSeconds(tossPaymentProperties.timeout());
+
+        factory.setConnectionRequestTimeout(timeout);
+        factory.setReadTimeout(timeout);
+
+        return factory;
     }
 }
