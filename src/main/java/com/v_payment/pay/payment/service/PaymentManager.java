8113453c828cee686903db85a3a978a73b0ff628
jeongjiho <wjdwlgh2000@naver.com>
2026-08-27 22:34:32 +0900

    refactor: 개별 쿼리로 나가는 pending_payments jdbcTemplate로 batch처리

--- src/main/java/com/v_payment/pay/payment/service/PaymentManager.java
+++ src/main/java/com/v_payment/pay/payment/service/PaymentManager.java
@@ -2,11 +2,15 @@ package com.v_payment.pay.payment.service;
 
 import com.v_payment.pay.payment.entity.Payment;
 import com.v_payment.pay.payment.entity.PaymentMethod;
+import com.v_payment.pay.payment.entity.PaymentStatus;
+import com.v_payment.pay.payment.entity.Provider;
 import com.v_payment.pay.payment.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.time.Clock;
+import java.time.LocalDateTime;
 import java.util.Collection;
 
 @Component
@@ -14,6 +18,7 @@ import java.util.Collection;
 public class PaymentManager {
     private final Clock clock;
     private final PaymentRepository paymentRepository;
+    private final JdbcTemplate jdbcTemplate;
 
     public void createPendingPayment(String orderCode, Long amount, PaymentMethod paymentMethod) {
         paymentRepository.save(Payment.createPendingPayment(orderCode, amount, paymentMethod, clock));
@@ -24,14 +29,34 @@ public class PaymentManager {
             return;
         }
 
-        paymentRepository.saveAll(requests.stream()
-                .map(request -> Payment.createPendingPayment(
-                        request.orderCode(),
-                        request.amount(),
-                        request.paymentMethod(),
-                        clock
-                ))
-                .toList());
+        LocalDateTime requestedAt = LocalDateTime.now(clock);
+        jdbcTemplate.batchUpdate(
+                """
+                insert into payment (
+                    provider,
+                    payment_method,
+                    order_code,
+                    payment_key,
+                    requested_amount,
+                    approved_amount,
+                    payment_status,
+                    requested_at,
+                    approved_at,
+                    receipt_url,
+                    recovery_attempt_count
+                ) values (?, ?, ?, null, ?, null, ?, ?, null, null, 0)
+                """,
+                requests,
+                requests.size(),
+                (ps, request) -> {
+                    ps.setString(1, Provider.TOSS.name());
+                    ps.setString(2, request.paymentMethod().name());
+                    ps.setString(3, request.orderCode());
+                    ps.setLong(4, request.amount());
+                    ps.setString(5, PaymentStatus.READY.name());
+                    ps.setObject(6, requestedAt);
+                }
+        );
     }
 
     public record PendingPaymentCreateRequest(
