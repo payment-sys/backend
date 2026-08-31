93cff06d1cc1664f9343ef32c053daf5deb7bfc7
chungjeongsu <wjdwlgh2000@gmail.com>
2026-08-31 14:40:18 +0900

    refactor: 주문 상품 정보를 값 객체로 분리

--- src/main/java/com/v_payment/pay/order/entity/OrderItem.java
+++ src/main/java/com/v_payment/pay/order/entity/OrderItem.java
@@ -1,6 +1,7 @@
 package com.v_payment.pay.order.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -9,12 +10,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "order_item")
 public class OrderItem {
     @Id
@@ -26,23 +28,37 @@ public class OrderItem {
     @JoinColumn(name = "order_id", nullable = false)
     private Order order;
 
-    private Long productId;
-
-    private String productName;
-
-    private Long unitPrice;
-
-    private Integer quantity;
-
-    private Long orderAmount;
+    @Embedded
+    private OrderItemInfo itemInfo;
 
     private OrderItem(Order order, Long productId, String productName, Long unitPrice, Integer quantity) {
         this.order = order;
-        this.productId = productId;
-        this.productName = productName;
-        this.unitPrice = unitPrice;
-        this.quantity = quantity;
-        this.orderAmount = unitPrice * quantity;
+        this.itemInfo = OrderItemInfo.create(productId, productName, unitPrice, quantity);
+    }
+
+    public Long getProductId() {
+        return itemInfo.getProductId();
+    }
+
+    public String getProductName() {
+        return itemInfo.getProductName();
+    }
+
+    public Integer getQuantity() {
+        return itemInfo.getQuantity();
+    }
+
+    public Long getUnitPrice() {
+        return itemInfo.getUnitPrice();
+    }
+
+    public Long getOrderAmount() {
+        return itemInfo.calculateOrderAmount();
+    }
+
+    public OrderItemInfo getOrderItemInfo() {    //방어적 복사
+        return OrderItemInfo.create(itemInfo.getProductId(), itemInfo.getProductName(), itemInfo.getUnitPrice(),
+                itemInfo.getQuantity());
     }
 
     public static OrderItem create(Order order, Long productId, String productName, Long unitPrice, Integer quantity) {
