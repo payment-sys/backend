ed5b3ea07f790f60603ab7fc0065312b2546ce70
chungjeongsu <wjdwlgh2000@gmail.com>
2026-09-10 13:39:14 +0900

    test: product 단위테스트 추가

--- src/main/java/com/v_payment/pay/product/entity/Product.java
+++ src/main/java/com/v_payment/pay/product/entity/Product.java
@@ -32,12 +32,29 @@ public class Product {
     private Integer stockQuantity;
 
     private Product(String name, Long price, Integer stockQuantity) {
-        this.name = name;
-        this.price = price;
-        this.stockQuantity = stockQuantity;
+        this.name = validateName(name);
+        this.price = validatePrice(price);
+        this.stockQuantity = validateStockQuantity(stockQuantity);
     }
 
     public static Product create(String name, Long price, Integer stockQuantity) {
         return new Product(name, price, stockQuantity);
+    }
+
+    private String validateName(String name) {
+        if (name == null || name.isBlank()) throw new IllegalArgumentException("name은 필수입니다.");
+        return name;
+    }
+
+    private Long validatePrice(Long price) {
+        if (price == null) throw new IllegalArgumentException("price는 null일 수 없습니다.");
+        if (price < 0) throw new IllegalArgumentException("price는 음수일 수 없습니다.");
+        return price;
+    }
+
+    private Integer validateStockQuantity(Integer stockQuantity) {
+        if (stockQuantity == null) throw new IllegalArgumentException("stockQuantity는 필수 입니다.");
+        if (stockQuantity < 0) throw new IllegalArgumentException("stockQuantity는 음수일 수 없습니다.");
+        return stockQuantity;
     }
 }
